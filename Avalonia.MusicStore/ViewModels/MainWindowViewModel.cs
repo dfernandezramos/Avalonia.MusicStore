ba11b1d232dc44e7f8d428564db08f55b9e8cc1a
David Fernández <dfernandez@fluendo.com>
2022-03-07 17:44:21 +0100

    Create a modern main window with transparencies

---
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace Avalonia.MusicStore.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            BuyMusicCommand = ReactiveCommand.Create(() =>
+            {
+                //
+            });
+        }
+        
+        public ICommand BuyMusicCommand { get; }
+    }
+}
