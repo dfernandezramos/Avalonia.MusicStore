22c4dee64820894d50cb676ce8a51edee71ceffe
David Fernández <dfernandez@fluendo.com>
2022-03-11 18:51:35 +0100

    Add content to music store window

--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,7 +1,39 @@
+using System.Collections.ObjectModel;
+using ReactiveUI;
+
 namespace Avalonia.MusicStore.ViewModels
 {
-    public class MusicStoreViewModel
+    public class MusicStoreViewModel : ViewModelBase
     {
-        
+        string? _searchText;
+        bool _isBusy;
+        AlbumViewModel? _selectedAlbum;
+
+        public MusicStoreViewModel()
+        {
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+            SearchResults.Add(new AlbumViewModel());
+        }
+
+        public string? SearchText
+        {
+            get => _searchText;
+            set => this.RaiseAndSetIfChanged(ref _searchText, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+        }
+
+        public AlbumViewModel? SelectedAlbum
+        {
+            get => _selectedAlbum;
+            set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+        }
+
+        public ObservableCollection<AlbumViewModel> SearchResults { get; } = new ObservableCollection<AlbumViewModel>();
     }
 }
