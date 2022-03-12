2cce58f650b7ecf56c82e4eb73b538b420cdcb83
David Fernández <dfernandez@fluendo.com>
2022-03-12 19:16:25 +0100

    Save purchased albums to user collection

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Reactive.Linq;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
 
@@ -6,6 +8,8 @@ namespace Avalonia.MusicStore.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        bool _collectionEmpty;
+
         public MainWindowViewModel()
         {
             ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
@@ -13,13 +17,28 @@ namespace Avalonia.MusicStore.ViewModels
             BuyMusicCommand = ReactiveCommand.Create(async () =>
             {
                 var store = new MusicStoreViewModel();
-
                 var result = await ShowDialog.Handle(store);
+
+                if (result != null)
+                {
+                    Albums.Add(result);
+                }
             });
+
+            this.WhenAnyValue(x => x.Albums.Count)
+                .Subscribe(x => CollectionEmpty = x == 0);
         }
         
         public ICommand BuyMusicCommand { get; }
 
         public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+
+        public bool CollectionEmpty
+        {
+            get => _collectionEmpty;
+            set => this.RaiseAndSetIfChanged(ref _collectionEmpty, value);
+        }
+
+        public ObservableCollection<AlbumViewModel> Albums { get; } = new ObservableCollection<AlbumViewModel>();
     }
 }
