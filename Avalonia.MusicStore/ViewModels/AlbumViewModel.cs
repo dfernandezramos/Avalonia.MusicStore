bbd2a2e50f619d149049f9133814b9cbefa764fb
David Fernández <dfernandez@fluendo.com>
2022-03-12 12:04:44 +0100

    Implement search API and render albums on UI

--- Avalonia.MusicStore/ViewModels/AlbumViewModel.cs
+++ Avalonia.MusicStore/ViewModels/AlbumViewModel.cs
@@ -1,7 +1,18 @@
+using Avalonia.MusicStore.Models;
+
 namespace Avalonia.MusicStore.ViewModels
 {
     public class AlbumViewModel : ViewModelBase
     {
-        
+        readonly Album _album;
+
+        public AlbumViewModel(Album album)
+        {
+            _album = album;
+        }
+
+        public string Artist => _album.Artist;
+
+        public string Title => _album.Title;
     }
 }
--- Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.ObjectModel;
 using System.Reactive;
+using System.Reactive.Linq;
+using Avalonia.MusicStore.Models;
 using ReactiveUI;
 
 namespace Avalonia.MusicStore.ViewModels
@@ -12,11 +15,12 @@ namespace Avalonia.MusicStore.ViewModels
 
         public MusicStoreViewModel()
         {
-            SearchResults.Add(new AlbumViewModel());
-            SearchResults.Add(new AlbumViewModel());
-            SearchResults.Add(new AlbumViewModel());
-
             BuyMusicCommand = ReactiveCommand.Create(() => SelectedAlbum);
+
+            this.WhenAnyValue(x => x.SearchText)
+                .Throttle(TimeSpan.FromMilliseconds(400))
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(DoSearch!);
         }
 
         public string? SearchText
@@ -40,5 +44,25 @@ namespace Avalonia.MusicStore.ViewModels
         public ObservableCollection<AlbumViewModel> SearchResults { get; } = new ObservableCollection<AlbumViewModel>();
 
         public ReactiveCommand<Unit, AlbumViewModel?> BuyMusicCommand { get; }
+
+        async void DoSearch(string s)
+        {
+            IsBusy = true;
+            SearchResults.Clear();
+
+            if (!string.IsNullOrWhiteSpace(s))
+            {
+                var albums = await Album.SearchAsync(s);
+
+                foreach (var album in albums)
+                {
+                    var vm = new AlbumViewModel(album);
+
+                    SearchResults.Add(vm);
+                }
+            }
+
+            IsBusy = false;
+        }
     }
 }
